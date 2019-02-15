217067647e2c9d00fdd3f427b060519550cf82a0
Woodland4678 <robotics@woodland.on.ca>
2019-02-14 20:51:54 -0500

    Intakes

--- src/main/cpp/Commands/IntakeControl.cpp
+++ src/main/cpp/Commands/IntakeControl.cpp
@@ -24,7 +24,12 @@ IntakeControl::IntakeControl(int direction): frc::Command() {
 
 // Called just before this Command runs the first time
 void IntakeControl::Initialize() {
-
+    if (m_direction == 1){
+        Robot::manipulatorArm->spinOutward(1);
+    }
+    else{
+        Robot::manipulatorArm->spinInward(1);
+    }
 }
 
 // Called repeatedly when this Command is scheduled to run
@@ -34,7 +39,7 @@ void IntakeControl::Execute() {
 
 // Make this return true when this Command no longer needs to run execute()
 bool IntakeControl::IsFinished() {
-    return false;
+    return true;
 }
 
 // Called once after isFinished returns true
